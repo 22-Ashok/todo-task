aa8fa5fb6d1b74bc68ce5eff2cd3ad73fa08e1e4
ashok <hs4486952@gmail.com>
2026-06-15 20:32:27 +0530

    make changes in url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 /**
  * Gets HTTP headers, appending authorization JWT token if present.
